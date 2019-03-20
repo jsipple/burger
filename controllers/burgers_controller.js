ab8a09daac7031534513d559f3747775dfa00fe8
unknown <sipple3@gmail.com>
2019-03-20 09:56:17 -0400

    fixed server to use router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,5 +13,25 @@ router.get('/', (req,res) => {
 })
 
 router.post('/api/burgers', (req,res) => {
-    burger.create([])
+burger.create(['burger_name'/*, devoured(don't think i need because default is false*/], [req.body.burger_name], result => {
+    res.json({ id: result.insertId})
 })
+})
+
+router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`
+    burger.update(
+        {
+            devoured: true
+        },
+        condition,
+        (result => {
+            if(result.changedRows === 0) {
+                return res.status(404).end()
+            }
+            res.status(200).end()
+        })
+    )
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,33 +1,21 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require('mysql');
-// Create an instance of the express app.
 const app = express();
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: '1234',
-//     database: 'bamazon'
-// })
 
 const PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const readData = () => {
-    /*connection.query(`select * from burgers`, (err, data) => {
-        if (err) throw err
-        data.burger_name
-    }) */
-}
+const routes = require('./controllers/burgers_controller')
 
-app.get('/', (req, res) => {
-    res.render('index', readData())
-})
+app.use(routes)
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
